cf39053a2ea46c666d1ea36e6f1153d4a225884a
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-24 14:44:37 +0200

    fix 1-install_flask.pp V0.4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 #!/usr/bin/pup
 # Install a specific version of flask (2.1.0)
-exec {'pup-install-flask':
-      command => '/usr/bin/apt-get -y install flask -v 2.1.0',
-        }
+
+package { 'python3-flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
+}
+
