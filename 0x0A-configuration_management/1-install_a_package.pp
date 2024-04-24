b69624ba5ca3ca43e3e0a96dab7d790a438f5f10
salmaa2005 <salmaamostafa2005@gmail.com>
2024-04-24 15:00:29 +0200

    fix execute-command task V0.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/puppet
 # Install a specific version of flask (2.1.0)
-exec { 'pup-install-flask':
+exec { 'flask':
   command => '/usr/bin/apt-get -y install flask=2.1.0',
   provider => 'pip3'
 }
